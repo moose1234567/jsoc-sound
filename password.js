196a70f5328faa4bae63725351c312e1824825b7
moose1234567 <94073156+moose1234567@users.noreply.github.com>
2022-08-12 14:51:19 -0400

    Add files via upload

---
+++ password.js
@@ -0,0 +1,16 @@
+var password = "12345";
+
+function passcheck(){
+
+if(document.getElementById('pass1').value != password){
+    alert('wrong password');
+    return false;
+
+ if(document,getElementById('pass1'),value == password){
+    alert('correct password');
+ }
+}
+
+
+
+}
